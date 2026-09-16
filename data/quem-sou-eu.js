5d70cf011088c14057bb25ec9227f853af963ead
Luan Chaves Barbosa <70718326+LuanChvs@users.noreply.github.com>
2026-09-16 18:26:46 -0300

    Adiciona dados iniciais do Quem Sou Eu

---
+++ data/quem-sou-eu.js
@@ -0,0 +1,132 @@
+window.DATA = window.DATA || {};
+
+window.DATA.quemSouEu = {
+  temas: [
+    {
+      id: 'pessoas',
+      nome: 'Pessoas',
+      nomes: [
+        'Albert Einstein',
+        'Ayrton Senna',
+        'Anitta',
+        'Pelé',
+        'Silvio Santos',
+        'Taylor Swift',
+        'Michael Jackson',
+        'Elon Musk'
+      ]
+    },
+    {
+      id: 'personagens',
+      nome: 'Personagens',
+      nomes: [
+        'Harry Potter',
+        'Homem-Aranha',
+        'Batman',
+        'Superman',
+        'Mickey Mouse',
+        'Bob Esponja',
+        'Shrek',
+        'Pikachu'
+      ]
+    },
+    {
+      id: 'profissoes',
+      nome: 'Profissões',
+      nomes: [
+        'Médico',
+        'Professor',
+        'Bombeiro',
+        'Piloto',
+        'Cozinheiro',
+        'Fotógrafo',
+        'Advogado',
+        'Astronauta'
+      ]
+    },
+    {
+      id: 'animais',
+      nome: 'Animais',
+      nomes: [
+        'Cachorro',
+        'Gato',
+        'Leão',
+        'Pinguim',
+        'Tubarão',
+        'Girafa',
+        'Macaco',
+        'Panda'
+      ]
+    },
+    {
+      id: 'objetos',
+      nome: 'Objetos',
+      nomes: [
+        'Guarda-chuva',
+        'Escova de dentes',
+        'Controle remoto',
+        'Óculos',
+        'Mochila',
+        'Relógio',
+        'Travesseiro',
+        'Câmera'
+      ]
+    },
+    {
+      id: 'lugares',
+      nome: 'Lugares',
+      nomes: [
+        'Praia',
+        'Paris',
+        'Disney',
+        'Aeroporto',
+        'Shopping',
+        'Escola',
+        'Academia',
+        'Restaurante'
+      ]
+    },
+    {
+      id: 'comidas',
+      nome: 'Comidas',
+      nomes: [
+        'Pizza',
+        'Hambúrguer',
+        'Sushi',
+        'Açaí',
+        'Pipoca',
+        'Brigadeiro',
+        'Coxinha',
+        'Sorvete'
+      ]
+    },
+    {
+      id: 'filmes-series',
+      nome: 'Filmes e séries',
+      nomes: [
+        'Stranger Things',
+        'Friends',
+        'The Office',
+        'Round 6',
+        'Wandinha',
+        'Breaking Bad',
+        'O Senhor dos Anéis',
+        'Toy Story'
+      ]
+    },
+    {
+      id: 'desenhos',
+      nome: 'Desenhos',
+      nomes: [
+        'Tom',
+        'Jerry',
+        'Pica-Pau',
+        'Scooby-Doo',
+        'Naruto',
+        'Goku',
+        'Ben 10',
+        'Homer Simpson'
+      ]
+    }
+  ]
+};
