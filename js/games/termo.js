c169f8e858878d61888dfe48f4c34c681a6d0961
Luan Chaves Barbosa <70718326+LuanChvs@users.noreply.github.com>
2026-09-18 11:37:47 -0300

    slow down Termo reveal animation

--- js/games/termo.js
+++ js/games/termo.js
@@ -280,10 +280,13 @@ function renderTermo() {
 
       window.setTimeout(() => {
         cell.classList.add(`is-${status}`);
+      }, 600);
+
+      window.setTimeout(() => {
         cell.classList.remove('is-revealing');
         cell.classList.add('is-filled');
-        window.setTimeout(() => revealNext(index + 1), 90);
-      }, 150);
+        window.setTimeout(() => revealNext(index + 1), 150);
+      }, 1200);
     };
 
     revealNext(0);
