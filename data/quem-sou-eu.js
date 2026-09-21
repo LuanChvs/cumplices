99e633630cf389bfcfeb48f999e73a892544ef03
Luan Chaves Barbosa <70718326+LuanChvs@users.noreply.github.com>
2026-09-21 15:04:07 -0300

    organiza catálogo do Quem Sou Eu em listas únicas

--- data/quem-sou-eu.js
+++ data/quem-sou-eu.js
@@ -16,7 +16,9 @@ window.DATA.quemSouEu = {
         'Lionel Messi','Ivete Sangalo','Xuxa','Roberto Carlos','Gisele Bündchen',
         'Fernanda Montenegro','Wagner Moura','Lázaro Ramos','Whindersson Nunes','Gustavo Lima',
         'Lady Gaga','Beyoncé','Rihanna','Keanu Reeves','Leonardo DiCaprio',
-        'Will Smith','Tom Cruise','Dwayne Johnson','Jackie Chan','Jim Carrey'
+        'Will Smith','Tom Cruise','Dwayne Johnson','Jackie Chan','Jim Carrey',
+        'Gal Gadot','Chris Hemsworth','Robert Downey Jr.','Ryan Reynolds','Emma Watson',
+        'Dua Lipa','Selena Gomez','Jennifer Lopez','Shakira','Alok'
       ]
     },
     {
@@ -28,7 +30,9 @@ window.DATA.quemSouEu = {
         'Thor','Wolverine','Deadpool','Mickey Mouse','Bob Esponja',
         'Shrek','Pikachu','Scooby-Doo','Salsicha','Goku',
         'Naruto','Sasuke','Luffy','Ichigo Kurosaki','Darth Vader',
-        'Luke Skywalker','Indiana Jones','Jack Sparrow','Homer Simpson','Pernalonga'
+        'Luke Skywalker','Indiana Jones','Jack Sparrow','Homer Simpson','Pernalonga',
+        'Elsa','Olaf','Woody','Buzz Lightyear','Simba',
+        'Timão','Pumba','Stitch','Mike Wazowski','Sulley'
       ]
     },
     {
@@ -40,7 +44,9 @@ window.DATA.quemSouEu = {
         'Engenheiro','Arquiteto','Programador','Designer','Veterinário',
         'Policial','Enfermeiro','Ator','Cantor','Músico',
         'Diretor de cinema','Chef de cozinha','Eletricista','Mecânico','Detetive',
-        'Repórter','Apresentador','Influenciador','Professor de educação física','Ilustrador'
+        'Repórter','Apresentador','Influenciador','Professor de educação física','Ilustrador',
+        'Biólogo','Químico','Físico','Cientista','Psicólogo',
+        'Nutricionista','Fisioterapeuta','Farmacêutico','Bancário','Contador'
       ]
     },
     {
@@ -52,7 +58,9 @@ window.DATA.quemSouEu = {
         'Urso','Lobo','Raposa','Coelho','Cavalo',
         'Zebra','Crocodilo','Jacaré','Gorila','Canguru',
         'Capivara','Preguiça','Golfinho','Baleia','Polvo',
-        'Águia','Coruja','Arara','Pavão','Camaleão'
+        'Águia','Coruja','Arara','Pavão','Camaleão',
+        'Hipopótamo','Rinoceronte','Camelo','Hiena','Morcego',
+        'Lontra','Castor','Esquilo','Tucano','Tamanduá'
       ]
     },
     {
@@ -64,7 +72,9 @@ window.DATA.quemSouEu = {
         'Lanterna Verde','Ciborgue','Arqueiro Verde','Supergirl','Batgirl',
         'Robin','Asa Noturna','Wolverine','Deadpool','Tempestade',
         'Professor Xavier','Magneto','Demolidor','Justiceiro','Pantera Negra',
-        'Doutor Estranho','Feiticeira Escarlate','Visão','Capitã Marvel','Groot'
+        'Doutor Estranho','Feiticeira Escarlate','Visão','Capitã Marvel','Groot',
+        'Homem-Formiga','Vespa','Gavião Arqueiro','Feiticeira','Elektra',
+        'Senhor Fantástico','Tocha Humana','Coisa','Jean Grey','Noturno'
       ]
     },
     {
@@ -76,7 +86,9 @@ window.DATA.quemSouEu = {
         'Lex Luthor','Arlequina','Pinguim','Charada','Mulher-Gato',
         'Darth Vader','Darth Maul','Imperador Palpatine','Voldemort','Bellatrix Lestrange',
         'Sauron','Saruman','Mestre Shen','Scar','Malévola',
-        'Cruella','Úrsula','Hades','Jafar','Agatha Harkness'
+        'Cruella','Úrsula','Hades','Jafar','Agatha Harkness',
+        'Mysterio','Abutre','Rei do Crime','Hela','Ego',
+        'Killmonger','Mística','Apocalipse','Ronan','Mandarim'
       ]
     },
     {
@@ -88,7 +100,9 @@ window.DATA.quemSouEu = {
         'Piratas do Caribe','Toy Story','Shrek','Frozen','Moana',
         'O Rei Leão','Procurando Nemo','Os Incríveis','Vingadores','Pantera Negra',
         'Homem-Aranha','Batman','Coringa','Barbie','Oppenheimer',
-        'Tropa de Elite','Cidade de Deus','Minha Mãe é uma Peça','As Branquelas','Esqueceram de Mim'
+        'Tropa de Elite','Cidade de Deus','Minha Mãe é uma Peça','As Branquelas','Esqueceram de Mim',
+        'O Iluminado','Forrest Gump','Clube da Luta','O Cavaleiro das Trevas','Jurassic World',
+        'Velozes e Furiosos','O Exterminador do Futuro','Rocky','Creed','O Máskara'
       ]
     },
     {
@@ -100,7 +114,9 @@ window.DATA.quemSouEu = {
         'The Boys','Loki','The Mandalorian','Game of Thrones','House',
         'Grey’s Anatomy','How I Met Your Mother','Brooklyn Nine-Nine','Cobra Kai','La Casa de Papel',
         'Narcos','Black Mirror','The Walking Dead','The Last of Us','The Big Bang Theory',
-        'Chaves','Todo Mundo Odeia o Chris','Eu, a Patroa e as Crianças','Um Maluco no Pedaço','Malcolm'
+        'Chaves','Todo Mundo Odeia o Chris','Eu, a Patroa e as Crianças','Um Maluco no Pedaço','Malcolm',
+        'Supernatural','The Witcher','Peaky Blinders','Suits','Vikings',
+        'Better Call Saul','Ozark','The Umbrella Academy','Alice in Borderland','Reacher'
       ]
     },
     {
@@ -112,7 +128,9 @@ window.DATA.quemSouEu = {
         'Homer Simpson','Bart Simpson','Lisa Simpson','Popeye','He-Man',
         'She-Ra','Ben 10','Coragem','Johnny Bravo','Dexter',
         'As Meninas Superpoderosas','Jimmy Neutron','Coiote','Papa-Léguas','Kung Fu Panda',
-        'Finn','Jake','Rick Sanchez','Morty Smith','Gumball'
+        'Finn','Jake','Rick Sanchez','Morty Smith','Gumball',
+        'O Máskara','As Aventuras de Jackie Chan','Du, Dudu e Edu','O Laboratório de Dexter','Kim Possible',
+        'Phineas e Ferb','Hora de Aventura','Steven Universo','Jovens Titãs','Avatar: A Lenda de Aang'
       ]
     },
     {
@@ -124,7 +142,9 @@ window.DATA.quemSouEu = {
         'Rukia Kuchiki','Gon Freecss','Killua Zoldyck','Hisoka','Kurapika',
         'Edward Elric','Alphonse Elric','Light Yagami','L','Tanjiro Kamado',
         'Nezuko Kamado','Satoru Gojo','Yuji Itadori','Eren Yeager','Mikasa Ackerman',
-        'Saitama','Genos','Izuku Midoriya','All Might','Levi Ackerman'
+        'Saitama','Genos','Izuku Midoriya','All Might','Levi Ackerman',
+        'Sasori','Itachi Uchiha','Frieza','Piccolo','Nico Robin',
+        'Boa Hancock'
       ]
     },
     {
@@ -136,7 +156,9 @@ window.DATA.quemSouEu = {
         'Temari','Kankuro','Jiraiya','Tsunade','Orochimaru',
         'Pain','Konan','Obito Uchiha','Madara Uchiha','Minato Namikaze',
         'Killer B','Might Guy','Deidara','Sasori','Kabuto Yakushi',
-        'Shino Aburame','Choji Akimichi','Ino Yamanaka','Kiba Inuzuka','Hiruzen Sarutobi'
+        'Shino Aburame','Choji Akimichi','Ino Yamanaka','Kiba Inuzuka','Hiruzen Sarutobi',
+        'Sai','Yamato','Kurenai Yuhi','Asuma Sarutobi','Konohamaru Sarutobi',
+        'Kisame Hoshigaki','Hidan','Kakuzu','Zabuza Momochi'
       ]
     },
     {
@@ -148,7 +170,9 @@ window.DATA.quemSouEu = {
         'Meruem','Neferpitou','Shaiapouf','Menthuthuyoupi','Komugi',
         'Isaac Netero','Silva Zoldyck','Zeno Zoldyck','Feitan','Machi',
         'Phinks','Shizuku','Nobunaga','Franklin','Bonolenov',
-        'Kalluto Zoldyck','Pakunoda','Uvogin','Palm Siberia','Knuckle Bine'
+        'Kalluto Zoldyck','Pakunoda','Uvogin','Palm Siberia','Knuckle Bine',
+        'Leorio Paradinight','Morel Mackernasey','Knov','Shoot McMahon','Genthru',
+        'Razor','Gotoh','Canary','Amane','Mito Freecss'
       ]
     },
     {
@@ -160,7 +184,9 @@ window.DATA.quemSouEu = {
         'Majin Boo','Mestre Kame','Yamcha','Tenshinhan','Chaos',
         'Android 18','Android 17','Android 16','Broly','Beerus',
         'Whis','Jiren','Hit','Videl','Mr. Satan',
-        'Chichi','Dende','Shenlong','Raditz','Nappa'
+        'Chichi','Dende','Shenlong','Raditz','Nappa',
+        'Uub','Android 19','Android 20','Dabura','Majin Vegeta',
+        'Kibito','Supremo Kaioh','Kaioshin','Zamasu'
       ]
     },
     {
@@ -172,7 +198,9 @@ window.DATA.quemSouEu = {
         'Ace','Sabo','Shanks','Buggy','Trafalgar Law',
         'Eustass Kid','Boa Hancock','Crocodile','Doflamingo','Kaido',
         'Big Mom','Barba Branca','Barba Negra','Gol D. Roger','Dracule Mihawk',
-        'Smoker','Tashigi','Akainu','Koby','Garp'
+        'Smoker','Tashigi','Akainu','Koby','Garp',
+        'Vivi','Perona','Rebecca','Yamato','Kozuki Oden',
+        'Donquixote Rosinante','Enel','Arlong','Momonosuke'
       ]
     },
     {
@@ -184,7 +212,9 @@ window.DATA.quemSouEu = {
         'Kisuke Urahara','Yoruichi Shihouin','Mayuri Kurotsuchi','Shunsui Kyoraku','Jushiro Ukitake',
         'Genryusai Yamamoto','Soi Fon','Rangiku Matsumoto','Ikkaku Madarame','Yumichika Ayasegawa',
         'Ulquiorra Cifer','Grimmjow Jaegerjaquez','Nelliel Tu Odelschwanck','Coyote Starrk','Nnoitra Gilga',
-        'Gin Ichimaru','Kaname Tosen','Isshin Kurosaki','White','Kon'
+        'Gin Ichimaru','Kaname Tosen','Isshin Kurosaki','White','Kon',
+        'Riruka Dokugamine','Tsukishima','Giselle Gewelle','Bambietta Basterbine','Jugram Haschwalth',
+        'Yhwach','Askin Nakk Le Vaar','Bazz-B','Retsu Unohana','Kensei Muguruma'
       ]
     },
     {
@@ -196,7 +226,9 @@ window.DATA.quemSouEu = {
         'Pikachu','Charizard','Mewtwo','Pac-Man','Kirby',
         'Mega Man','Samus Aran','Master Chief','Lara Croft','Kratos',
         'Nathan Drake','Steve','Creeper','Sub-Zero','Scorpion',
-        'Ryu','Ken','Chun-Li','Crash Bandicoot','Spyro'
+        'Ryu','Ken','Chun-Li','Crash Bandicoot','Spyro',
+        'Wario','Waluigi','Yoshi','Daisy','Toad',
+        'Fox McCloud','Captain Falcon','Jigglypuff','Mortal Kombat'
       ]
     },
     {
@@ -208,7 +240,9 @@ window.DATA.quemSouEu = {
         'Zico','Marta','Formiga','Cristiano Ronaldo','Lionel Messi',
         'Kylian Mbappé','Erling Haaland','Vinícius Júnior','Rodrygo','Gabriel Jesus',
         'Ronaldinho','Zlatan Ibrahimović','David Beckham','Thierry Henry','Andrés Iniesta',
-        'Xavi','Johan Cruyff','Diego Maradona','Manuel Neuer','Neymar Jr.'
+        'Xavi','Johan Cruyff','Diego Maradona','Manuel Neuer','Neymar Jr.',
+        'Romelu Lukaku','Kevin De Bruyne','Mohamed Salah','Sadio Mané','Gabriel Martinelli',
+        'Endrick','Casemiro','Thiago Silva','Alisson'
       ]
     },
     {
@@ -218,158 +252,4 @@ window.DATA.quemSouEu = {
         'Michael Jackson','Madonna','Elvis Presley','Freddie Mercury','Beyoncé',
         'Rihanna','Lady Gaga','Taylor Swift','Adele','Bruno Mars',
         'Justin Bieber','The Weeknd','Ed Sheeran','Ariana Grande','Katy Perry',
-        'Anitta','Ivete Sangalo','Caetano Veloso','Gilberto Gil','Roberto Carlos',
-        'Tim Maia','Cazuza','Renato Russo','Legião Urbana','Charlie Brown Jr.',
-        'Mamonas Assassinas','Marília Mendonça','Gustavo Lima','Luan Santana','Jorge e Mateus'
-      ]
-    },
-    {
-      id: 'internet',
-      nome: 'Internet e famosos',
-      nomes: [
-        'Whindersson Nunes','Felipe Neto','Casimiro','Bruno PlayHard','Cellbit',
-        'Nobru','Gaules','Alanzoka','Cocielo','KondZilla',
-        'Luccas Neto','Enaldinho','Virginia Fonseca','Bianca Andrade','Camila Loures',
-        'Kéfera','Rezende','AuthenticGames','BRKsEDU','Manual do Mundo',
-        'MrBeast','PewDiePie','Markiplier','Ninja','Logan Paul',
-        'KSI','Charli D’Amelio','Khaby Lame','Mrwhosetheboss','IShowSpeed'
-      ]
-    },
-    {
-      id: 'historia',
-      nome: 'História',
-      nomes: [
-        'Dom Pedro I','Dom Pedro II','Tiradentes','Getúlio Vargas','Juscelino Kubitschek',
-        'Santos Dumont','Princesa Isabel','Marechal Deodoro','Machado de Assis','Zumbi dos Palmares',
-        'Cleópatra','Júlio César','Alexandre o Grande','Napoleão Bonaparte','Joana d’Arc',
-        'Leonardo da Vinci','Michelangelo','Galileu Galilei','Isaac Newton','Marie Curie',
-        'Charles Darwin','Mahatma Gandhi','Nelson Mandela','Martin Luther King Jr.','Abraham Lincoln',
-        'George Washington','Cristóvão Colombo','Marco Polo','Rainha Elizabeth II','Vasco da Gama'
-      ]
-    },
-    {
-      id: 'mitologia',
-      nome: 'Mitologia',
-      nomes: [
-        'Zeus','Hera','Poseidon','Hades','Atena',
-        'Ares','Afrodite','Apolo','Ártemis','Hermes',
-        'Hefesto','Dionísio','Medusa','Minotauro','Hércules',
-        'Aquiles','Odisseu','Perseu','Teseu','Prometeu',
-        'Thor','Loki','Odin','Freya','Anúbis',
-        'Rá','Ísis','Osíris','Anúbis','Quetzalcóatl'
-      ]
-    },
-    {
-      id: 'brasil',
-      nome: 'Brasil',
-      nomes: [
-        'Ayrton Senna','Pelé','Silvio Santos','Xuxa','Anitta',
-        'Ivete Sangalo','Roberto Carlos','Chacrinha','Jô Soares','Faustão',
-        'Ratinho','Luciano Huck','Sabrina Sato','Tatá Werneck','Paulo Gustavo',
-        'Whindersson Nunes','Neymar','Marta','Ronaldinho Gaúcho','Gisele Bündchen',
-        'Fernanda Montenegro','Wagner Moura','Selton Mello','Rodrigo Santoro','Lázaro Ramos',
-        'Caetano Veloso','Gilberto Gil','Renato Russo','Marília Mendonça','Santos Dumont'
-      ]
-    }
-  ]
-};
-/* =========================================================
-   EXPANSÃO DO CATÁLOGO — mínimo de 40 itens por tema
-========================================================= */
-
-const QUEM_SOU_EU_EXTRAS = {
-  pessoas: [
-    'Gal Gadot','Chris Hemsworth','Robert Downey Jr.','Ryan Reynolds','Emma Watson',
-    'Dua Lipa','Selena Gomez','Jennifer Lopez','Shakira','Alok'
-  ],
-  personagens: [
-    'Elsa','Olaf','Woody','Buzz Lightyear','Simba',
-    'Timão','Pumba','Stitch','Mike Wazowski','Sulley'
-  ],
-  profissoes: [
-    'Biólogo','Químico','Físico','Cientista','Psicólogo',
-    'Nutricionista','Fisioterapeuta','Farmacêutico','Bancário','Contador'
-  ],
-  animais: [
-    'Hipopótamo','Rinoceronte','Camelo','Hiena','Morcego',
-    'Lontra','Castor','Esquilo','Tucano','Tamanduá'
-  ],
-  'super-herois': [
-    'Homem-Formiga','Vespa','Gavião Arqueiro','Feiticeira','Elektra',
-    'Senhor Fantástico','Tocha Humana','Coisa','Jean Grey','Noturno'
-  ],
-  viloes: [
-    'Mysterio','Abutre','Rei do Crime','Hela','Ego',
-    'Killmonger','Mística','Apocalipse','Ronan','Mandarim'
-  ],
-  filmes: [
-    'O Iluminado','Forrest Gump','Clube da Luta','O Cavaleiro das Trevas','Jurassic World',
-    'Velozes e Furiosos','O Exterminador do Futuro','Rocky','Creed','O Máskara'
-  ],
-  series: [
-    'Supernatural','The Witcher','Peaky Blinders','Suits','Vikings',
-    'Better Call Saul','Ozark','The Umbrella Academy','Alice in Borderland','Reacher'
-  ],
-  desenhos: [
-    'O Máskara','As Aventuras de Jackie Chan','Du, Dudu e Edu','O Laboratório de Dexter','Kim Possible',
-    'Phineas e Ferb','Hora de Aventura','Steven Universo','Jovens Titãs','Avatar: A Lenda de Aang'
-  ],
-  anime: [
-    'Sasori','Itachi Uchiha','Frieza','Piccolo','Vegeta',
-    'Nico Robin','Boa Hancock','Rukia Kuchiki','Killua Zoldyck','Satoru Gojo'
-  ],
-  naruto: [
-    'Sai','Yamato','Kurenai Yuhi','Asuma Sarutobi','Might Guy',
-    'Konohamaru Sarutobi','Kisame Hoshigaki','Hidan','Kakuzu','Zabuza Momochi'
-  ],
-  'hunter-x-hunter': [
-    'Leorio Paradinight','Morel Mackernasey','Knov','Shoot McMahon','Genthru',
-    'Razor','Gotoh','Canary','Amane','Mito Freecss'
-  ],
-  'dragon-ball': [
-    'Uub','Android 19','Android 20','Dabura','Majin Vegeta',
-    'Kibito','Supremo Kaioh','Kaioshin','Raditz','Zamasu'
-  ],
-  'one-piece': [
-    'Vivi','Perona','Rebecca','Yamato','Kozuki Oden',
-    'Donquixote Rosinante','Enel','Arlong','Boa Hancock','Momonosuke'
-  ],
-  bleach: [
-    'Riruka Dokugamine','Tsukishima','Giselle Gewelle','Bambietta Basterbine','Jugram Haschwalth',
-    'Yhwach','Askin Nakk Le Vaar','Bazz-B','Retsu Unohana','Kensei Muguruma'
-  ],
-  jogos: [
-    'Wario','Waluigi','Yoshi','Daisy','Toad',
-    'Fox McCloud','Captain Falcon','Jigglypuff','Pikachu','Mortal Kombat'
-  ],
-  futebol: [
-    'Romelu Lukaku','Kevin De Bruyne','Mohamed Salah','Sadio Mané','Neymar Jr.',
-    'Gabriel Martinelli','Endrick','Casemiro','Thiago Silva','Alisson'
-  ],
-  musica: [
-    'Elton John','David Bowie','Prince','Bob Marley','Amy Winehouse',
-    'Justin Timberlake','Sia','Miley Cyrus','Olivia Rodrigo','Post Malone'
-  ],
-  internet: [
-    'Felipe Neto','Podpah','Igão','Mítico','Bruno Correa',
-    'CazéTV','Rato Borrachudo','Jovem Nerd','Azaghal','Rafinha Bastos'
-  ],
-  historia: [
-    'Simón Bolívar','Che Guevara','Fidel Castro','Winston Churchill','Adolf Hitler',
-    'Karl Marx','Vladimir Lenin','Rainha Vitória','Cleópatra','Confúcio'
-  ],
-  mitologia: [
-    'Fenrir','Jörmungandr','Hel','Balder','Valquíria',
-    'Cérbero','Hidra de Lerna','Esfinge','Ícaro','Midas'
-  ],
-  brasil: [
-    'Renata Fan','Galvão Bueno','Ana Maria Braga','Regina Casé','Marcos Mion',
-    'Fátima Bernardes','Glória Maria','Cássia Kis','Leandro Hassum','Tiririca'
-  ]
-};
-
-Object.entries(QUEM_SOU_EU_EXTRAS).forEach(([id, extras]) => {
-  const tema = window.DATA.quemSouEu.temas.find(item => item.id === id);
-  if (!tema) return;
-  tema.nomes = [...new Set([...tema.nomes, ...extras])];
-});
+        'Anitta','Ivet
