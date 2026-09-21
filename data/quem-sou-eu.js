0245a4be856a8663565f259df89e0030f3b98903
Luan Chaves Barbosa <70718326+LuanChvs@users.noreply.github.com>
2026-09-21 14:53:34 -0300

    feat: ampliar banco de nomes do Quem Sou Eu

--- data/quem-sou-eu.js
+++ data/quem-sou-eu.js
@@ -1,3 +1,8 @@
+/* =========================================================
+   QUEM SOU EU — BANCO DE NOMES
+   Catálogo amplo de pessoas, personagens e figuras conhecidas.
+========================================================= */
+
 window.DATA = window.DATA || {};
 
 window.DATA.quemSouEu = {
@@ -6,126 +11,264 @@ window.DATA.quemSouEu = {
       id: 'pessoas',
       nome: 'Pessoas',
       nomes: [
-        'Albert Einstein',
-        'Ayrton Senna',
-        'Anitta',
-        'Pelé',
-        'Silvio Santos',
-        'Taylor Swift',
-        'Michael Jackson',
-        'Elon Musk'
+        'Albert Einstein','Ayrton Senna','Anitta','Pelé','Silvio Santos',
+        'Taylor Swift','Michael Jackson','Elon Musk','Neymar','Cristiano Ronaldo',
+        'Lionel Messi','Ivete Sangalo','Xuxa','Roberto Carlos','Gisele Bündchen',
+        'Fernanda Montenegro','Wagner Moura','Lázaro Ramos','Whindersson Nunes','Gustavo Lima',
+        'Lady Gaga','Beyoncé','Rihanna','Keanu Reeves','Leonardo DiCaprio',
+        'Will Smith','Tom Cruise','Dwayne Johnson','Jackie Chan','Jim Carrey'
       ]
     },
     {
       id: 'personagens',
       nome: 'Personagens',
       nomes: [
-        'Harry Potter',
-        'Homem-Aranha',
-        'Batman',
-        'Superman',
-        'Mickey Mouse',
-        'Bob Esponja',
-        'Shrek',
-        'Pikachu'
+        'Harry Potter','Hermione Granger','Ron Weasley','Homem-Aranha','Batman',
+        'Superman','Mulher-Maravilha','Homem de Ferro','Capitão América','Hulk',
+        'Thor','Wolverine','Deadpool','Mickey Mouse','Bob Esponja',
+        'Shrek','Pikachu','Scooby-Doo','Salsicha','Goku',
+        'Naruto','Sasuke','Luffy','Ichigo Kurosaki','Darth Vader',
+        'Luke Skywalker','Indiana Jones','Jack Sparrow','Homer Simpson','Pernalonga'
       ]
     },
     {
       id: 'profissoes',
       nome: 'Profissões',
       nomes: [
-        'Médico',
-        'Professor',
-        'Bombeiro',
-        'Piloto',
-        'Cozinheiro',
-        'Fotógrafo',
-        'Advogado',
-        'Astronauta'
+        'Médico','Professor','Bombeiro','Piloto','Cozinheiro',
+        'Fotógrafo','Advogado','Astronauta','Dentista','Jornalista',
+        'Engenheiro','Arquiteto','Programador','Designer','Veterinário',
+        'Policial','Enfermeiro','Ator','Cantor','Músico',
+        'Diretor de cinema','Chef de cozinha','Eletricista','Mecânico','Detetive',
+        'Repórter','Apresentador','Influenciador','Professor de educação física','Ilustrador'
       ]
     },
     {
       id: 'animais',
       nome: 'Animais',
       nomes: [
-        'Cachorro',
-        'Gato',
-        'Leão',
-        'Pinguim',
-        'Tubarão',
-        'Girafa',
-        'Macaco',
-        'Panda'
+        'Cachorro','Gato','Leão','Pinguim','Tubarão',
+        'Girafa','Macaco','Panda','Elefante','Tigre',
+        'Urso','Lobo','Raposa','Coelho','Cavalo',
+        'Zebra','Crocodilo','Jacaré','Gorila','Canguru',
+        'Capivara','Preguiça','Golfinho','Baleia','Polvo',
+        'Águia','Coruja','Arara','Pavão','Camaleão'
       ]
     },
     {
-      id: 'objetos',
-      nome: 'Objetos',
+      id: 'super-herois',
+      nome: 'Super-heróis',
       nomes: [
-        'Guarda-chuva',
-        'Escova de dentes',
-        'Controle remoto',
-        'Óculos',
-        'Mochila',
-        'Relógio',
-        'Travesseiro',
-        'Câmera'
+        'Homem-Aranha','Homem de Ferro','Capitão América','Thor','Hulk',
+        'Batman','Superman','Mulher-Maravilha','Flash','Aquaman',
+        'Lanterna Verde','Ciborgue','Arqueiro Verde','Supergirl','Batgirl',
+        'Robin','Asa Noturna','Wolverine','Deadpool','Tempestade',
+        'Professor Xavier','Magneto','Demolidor','Justiceiro','Pantera Negra',
+        'Doutor Estranho','Feiticeira Escarlate','Visão','Capitã Marvel','Groot'
       ]
     },
     {
-      id: 'lugares',
-      nome: 'Lugares',
+      id: 'viloes',
+      nome: 'Vilões',
       nomes: [
-        'Praia',
-        'Paris',
-        'Disney',
-        'Aeroporto',
-        'Shopping',
-        'Escola',
-        'Academia',
-        'Restaurante'
+        'Coringa','Thanos','Loki','Duende Verde','Doutor Octopus',
+        'Venom','Caveira Vermelha','Ultron','Magneto','Doutor Destino',
+        'Lex Luthor','Arlequina','Pinguim','Charada','Mulher-Gato',
+        'Darth Vader','Darth Maul','Imperador Palpatine','Voldemort','Bellatrix Lestrange',
+        'Sauron','Saruman','Mestre Shen','Scar','Malévola',
+        'Cruella','Úrsula','Hades','Jafar','Agatha Harkness'
       ]
     },
     {
-      id: 'comidas',
-      nome: 'Comidas',
+      id: 'filmes',
+      nome: 'Filmes',
       nomes: [
-        'Pizza',
-        'Hambúrguer',
-        'Sushi',
-        'Açaí',
-        'Pipoca',
-        'Brigadeiro',
-        'Coxinha',
-        'Sorvete'
+        'Titanic','Avatar','Matrix','Interestelar','Gladiador',
+        'O Poderoso Chefão','O Senhor dos Anéis','Harry Potter','Jurassic Park','De Volta para o Futuro',
+        'Piratas do Caribe','Toy Story','Shrek','Frozen','Moana',
+        'O Rei Leão','Procurando Nemo','Os Incríveis','Vingadores','Pantera Negra',
+        'Homem-Aranha','Batman','Coringa','Barbie','Oppenheimer',
+        'Tropa de Elite','Cidade de Deus','Minha Mãe é uma Peça','As Branquelas','Esqueceram de Mim'
       ]
     },
     {
-      id: 'filmes-series',
-      nome: 'Filmes e séries',
+      id: 'series',
+      nome: 'Séries',
       nomes: [
-        'Stranger Things',
-        'Friends',
-        'The Office',
-        'Round 6',
-        'Wandinha',
-        'Breaking Bad',
-        'O Senhor dos Anéis',
-        'Toy Story'
+        'Friends','The Office','Breaking Bad','Stranger Things','Round 6',
+        'Wandinha','Dark','Lost','Dexter','Prison Break',
+        'The Boys','Loki','The Mandalorian','Game of Thrones','House',
+        'Grey’s Anatomy','How I Met Your Mother','Brooklyn Nine-Nine','Cobra Kai','La Casa de Papel',
+        'Narcos','Black Mirror','The Walking Dead','The Last of Us','The Big Bang Theory',
+        'Chaves','Todo Mundo Odeia o Chris','Eu, a Patroa e as Crianças','Um Maluco no Pedaço','Malcolm'
       ]
     },
     {
       id: 'desenhos',
       nome: 'Desenhos',
       nomes: [
-        'Tom',
-        'Jerry',
-        'Pica-Pau',
-        'Scooby-Doo',
-        'Naruto',
-        'Goku',
-        'Ben 10',
-        'Homer Simpson'
+        'Mickey Mouse','Pernalonga','Patolino','Pica-Pau','Tom',
+        'Jerry','Scooby-Doo','Salsicha','Bob Esponja','Patrick Estrela',
+        'Homer Simpson','Bart Simpson','Lisa Simpson','Popeye','He-Man',
+        'She-Ra','Ben 10','Coragem','Johnny Bravo','Dexter',
+        'As Meninas Superpoderosas','Jimmy Neutron','Coiote','Papa-Léguas','Kung Fu Panda',
+        'Finn','Jake','Rick Sanchez','Morty Smith','Gumball'
+      ]
+    },
+    {
+      id: 'anime',
+      nome: 'Anime',
+      nomes: [
+        'Naruto','Sasuke','Kakashi','Goku','Vegeta',
+        'Luffy','Zoro','Sanji','Nami','Ichigo Kurosaki',
+        'Rukia Kuchiki','Gon Freecss','Killua Zoldyck','Hisoka','Kurapika',
+        'Edward Elric','Alphonse Elric','Light Yagami','L','Tanjiro Kamado',
+        'Nezuko Kamado','Satoru Gojo','Yuji Itadori','Eren Yeager','Mikasa Ackerman',
+        'Saitama','Genos','Izuku Midoriya','All Might','Levi Ackerman'
+      ]
+    },
+    {
+      id: 'naruto',
+      nome: 'Naruto',
+      nomes: [
+        'Naruto Uzumaki','Sasuke Uchiha','Sakura Haruno','Kakashi Hatake','Itachi Uchiha',
+        'Shikamaru Nara','Hinata Hyuga','Neji Hyuga','Rock Lee','Gaara',
+        'Temari','Kankuro','Jiraiya','Tsunade','Orochimaru',
+        'Pain','Konan','Obito Uchiha','Madara Uchiha','Minato Namikaze',
+        'Killer B','Might Guy','Deidara','Sasori','Kabuto Yakushi',
+        'Shino Aburame','Choji Akimichi','Ino Yamanaka','Kiba Inuzuka','Hiruzen Sarutobi'
+      ]
+    },
+    {
+      id: 'hunter-x-hunter',
+      nome: 'Hunter x Hunter',
+      nomes: [
+        'Gon Freecss','Killua Zoldyck','Kurapika','Leorio','Hisoka',
+        'Chrollo Lucilfer','Illumi Zoldyck','Biscuit Krueger','Kite','Ging Freecss',
+        'Meruem','Neferpitou','Shaiapouf','Menthuthuyoupi','Komugi',
+        'Isaac Netero','Silva Zoldyck','Zeno Zoldyck','Feitan','Machi',
+        'Phinks','Shizuku','Nobunaga','Franklin','Bonolenov',
+        'Kalluto Zoldyck','Pakunoda','Uvogin','Palm Siberia','Knuckle Bine'
+      ]
+    },
+    {
+      id: 'dragon-ball',
+      nome: 'Dragon Ball',
+      nomes: [
+        'Goku','Vegeta','Gohan','Piccolo','Kuririn',
+        'Bulma','Trunks','Goten','Freeza','Cell',
+        'Majin Boo','Mestre Kame','Yamcha','Tenshinhan','Chaos',
+        'Android 18','Android 17','Android 16','Broly','Beerus',
+        'Whis','Jiren','Hit','Videl','Mr. Satan',
+        'Chichi','Dende','Shenlong','Raditz','Nappa'
+      ]
+    },
+    {
+      id: 'one-piece',
+      nome: 'One Piece',
+      nomes: [
+        'Luffy','Zoro','Nami','Usopp','Sanji',
+        'Tony Tony Chopper','Nico Robin','Franky','Brook','Jinbe',
+        'Ace','Sabo','Shanks','Buggy','Trafalgar Law',
+        'Eustass Kid','Boa Hancock','Crocodile','Doflamingo','Kaido',
+        'Big Mom','Barba Branca','Barba Negra','Gol D. Roger','Dracule Mihawk',
+        'Smoker','Tashigi','Akainu','Koby','Garp'
+      ]
+    },
+    {
+      id: 'bleach',
+      nome: 'Bleach',
+      nomes: [
+        'Ichigo Kurosaki','Rukia Kuchiki','Orihime Inoue','Uryu Ishida','Yasutora Sado',
+        'Renji Abarai','Byakuya Kuchiki','Toshiro Hitsugaya','Kenpachi Zaraki','Sosuke Aizen',
+        'Kisuke Urahara','Yoruichi Shihouin','Mayuri Kurotsuchi','Shunsui Kyoraku','Jushiro Ukitake',
+        'Genryusai Yamamoto','Soi Fon','Rangiku Matsumoto','Ikkaku Madarame','Yumichika Ayasegawa',
+        'Ulquiorra Cifer','Grimmjow Jaegerjaquez','Nelliel Tu Odelschwanck','Coyote Starrk','Nnoitra Gilga',
+        'Gin Ichimaru','Kaname Tosen','Isshin Kurosaki','White','Kon'
+      ]
+    },
+    {
+      id: 'jogos',
+      nome: 'Jogos',
+      nomes: [
+        'Mario','Luigi','Princess Peach','Bowser','Link',
+        'Zelda','Donkey Kong','Sonic','Tails','Knuckles',
+        'Pikachu','Charizard','Mewtwo','Pac-Man','Kirby',
+        'Mega Man','Samus Aran','Master Chief','Lara Croft','Kratos',
+        'Nathan Drake','Steve','Creeper','Sub-Zero','Scorpion',
+        'Ryu','Ken','Chun-Li','Crash Bandicoot','Spyro'
+      ]
+    },
+    {
+      id: 'futebol',
+      nome: 'Futebol',
+      nomes: [
+        'Pelé','Neymar','Ronaldinho Gaúcho','Ronaldo Fenômeno','Romário',
+        'Kaká','Rivaldo','Cafu','Roberto Carlos','Garrincha',
+        'Zico','Marta','Formiga','Cristiano Ronaldo','Lionel Messi',
+        'Kylian Mbappé','Erling Haaland','Vinícius Júnior','Rodrygo','Gabriel Jesus',
+        'Ronaldinho','Zlatan Ibrahimović','David Beckham','Thierry Henry','Andrés Iniesta',
+        'Xavi','Johan Cruyff','Diego Maradona','Manuel Neuer','Neymar Jr.'
+      ]
+    },
+    {
+      id: 'musica',
+      nome: 'Música',
+      nomes: [
+        'Michael Jackson','Madonna','Elvis Presley','Freddie Mercury','Beyoncé',
+        'Rihanna','Lady Gaga','Taylor Swift','Adele','Bruno Mars',
+        'Justin Bieber','The Weeknd','Ed Sheeran','Ariana Grande','Katy Perry',
+        'Anitta','Ivete Sangalo','Caetano Veloso','Gilberto Gil','Roberto Carlos',
+        'Tim Maia','Cazuza','Renato Russo','Legião Urbana','Charlie Brown Jr.',
+        'Mamonas Assassinas','Marília Mendonça','Gustavo Lima','Luan Santana','Jorge e Mateus'
+      ]
+    },
+    {
+      id: 'internet',
+      nome: 'Internet e famosos',
+      nomes: [
+        'Whindersson Nunes','Felipe Neto','Casimiro','Bruno PlayHard','Cellbit',
+        'Nobru','Gaules','Alanzoka','Cocielo','KondZilla',
+        'Luccas Neto','Enaldinho','Virginia Fonseca','Bianca Andrade','Camila Loures',
+        'Kéfera','Rezende','AuthenticGames','BRKsEDU','Manual do Mundo',
+        'MrBeast','PewDiePie','Markiplier','Ninja','Logan Paul',
+        'KSI','Charli D’Amelio','Khaby Lame','Mrwhosetheboss','IShowSpeed'
+      ]
+    },
+    {
+      id: 'historia',
+      nome: 'História',
+      nomes: [
+        'Dom Pedro I','Dom Pedro II','Tiradentes','Getúlio Vargas','Juscelino Kubitschek',
+        'Santos Dumont','Princesa Isabel','Marechal Deodoro','Machado de Assis','Zumbi dos Palmares',
+        'Cleópatra','Júlio César','Alexandre o Grande','Napoleão Bonaparte','Joana d’Arc',
+        'Leonardo da Vinci','Michelangelo','Galileu Galilei','Isaac Newton','Marie Curie',
+        'Charles Darwin','Mahatma Gandhi','Nelson Mandela','Martin Luther King Jr.','Abraham Lincoln',
+        'George Washington','Cristóvão Colombo','Marco Polo','Rainha Elizabeth II','Vasco da Gama'
+      ]
+    },
+    {
+      id: 'mitologia',
+      nome: 'Mitologia',
+      nomes: [
+        'Zeus','Hera','Poseidon','Hades','Atena',
+        'Ares','Afrodite','Apolo','Ártemis','Hermes',
+        'Hefesto','Dionísio','Medusa','Minotauro','Hércules',
+        'Aquiles','Odisseu','Perseu','Teseu','Prometeu',
+        'Thor','Loki','Odin','Freya','Anúbis',
+        'Rá','Ísis','Osíris','Anúbis','Quetzalcóatl'
+      ]
+    },
+    {
+      id: 'brasil',
+      nome: 'Brasil',
+      nomes: [
+        'Ayrton Senna','Pelé','Silvio Santos','Xuxa','Anitta',
+        'Ivete Sangalo','Roberto Carlos','Chacrinha','Jô Soares','Faustão',
+        'Ratinho','Luciano Huck','Sabrina Sato','Tatá Werneck','Paulo Gustavo',
+        'Whindersson Nunes','Neymar','Marta','Ronaldinho Gaúcho','Gisele Bündchen',
+        'Fernanda Montenegro','Wagner Moura','Selton Mello','Rodrigo Santoro','Lázaro Ramos',
+        'Caetano Veloso','Gilberto Gil','Renato Russo','Marília Mendonça','Santos Dumont'
       ]
     }
   ]
